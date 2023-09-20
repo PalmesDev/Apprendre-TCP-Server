45f0fb4ecc78b530fd5e0f47110a485c7db4e949
Palmes <77801937+PalmesDev@users.noreply.github.com>
2023-09-20 12:33:47 +0200

    push

--- Serveur tcp/SocketClient/client.cs
+++ Serveur tcp/SocketClient/client.cs
@@ -6,17 +6,19 @@ Console.WriteLine("Client starting...");
 
 IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
 IPAddress ipAddress = ipHostInfo.AddressList[0];
-
 IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 54265);
 // Definit une adresse IP serveur ainsi que son port de connexion.
+
 Console.WriteLine($"Adresse du serveur utilisant l'ip : \"{ipAddress.MapToIPv4()}\" sur le port : \"{iPEndPoint.Port}\" trouvée !");
+
 using Socket client = new(iPEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 // Definit un socket avec comme parametre l'ip, le type de socket (ici stream pour une communication send/recive), et le type de protocol (ici TCP) permettant la connexion au serveur distant.
 
 await client.ConnectAsync(iPEndPoint);
-// L'opérateur await permet d'attendre la connexion asyncrone(?) du client vers le serveur. Lorsque le client est connecter, await n'attend plus la connexion.
+// L'opérateur await permet d'attendre la connexion asyncrone* du client vers le serveur. Lorsque le client est connecté, await n'attend plus de connexion et passe au reste du program.
+// *La connexion asyncrone est un type de connexion "différé".
 
-while (true)
+while (true) // Tant que la connexion est valide
 {
 
 
@@ -28,7 +30,7 @@ while (true)
     var messageBytes = Encoding.UTF8.GetBytes(message);
     // Encodage du message en UTF8 (Binaire).
     await client.SendAsync(messageBytes, SocketFlags.None);
-    // Envoie le message et attend l'accusé de reception. 
+    // Envoie le message et attend que le serveur est reçu le message.
     Console.WriteLine($"Le client envoie le message : \"{message}\"");
     // Affichage du message envoyer par le serveur dans la console.
 
@@ -42,7 +44,8 @@ while (true)
     // Attend et recoit le message dans le buffer 
     var reponse = Encoding.UTF8.GetString(buffer, 0, received);
     // Décode le message dans le buffer 
-    if(reponse == "Hello Client !")
+
+    if(reponse == "Hello Client !") // Si le message envoyé par le serveur est identique.
     {
         Console.WriteLine($"Le client a reçu l'accusé de reception : \"{reponse}\"");
         break;
@@ -50,4 +53,4 @@ while (true)
 
 }
 
-client.Shutdown(SocketShutdown.Both);
+client.Shutdown(SocketShutdown.Both); // Désactive la connexion du client (Both pour arrêter l'envoi et la réception de donnée)
--- Serveur tcp/SocketServer/server.cs
+++ Serveur tcp/SocketServer/server.cs
@@ -6,18 +6,21 @@ Console.WriteLine("Serveur starting...");
 
 IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
 IPAddress ipAddress = ipHostInfo.AddressList[0];
-
 IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 54265);
 
 using Socket listener = new(iPEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+// Défini un socket server avec comme paramètres l'ip, le type de socket ainsi que le protocol.
 
 listener.Bind(iPEndPoint);
+// Liaison du socket server à l'ip d'écoute. 
 listener.Listen(100);
+// Met le socket server en ecoute 
 Console.WriteLine("Serveur démarré, en attente du client...");
+
 var handler = await listener.AcceptAsync();
+// Attend la connexion avec le client
 
-
-while (true)
+while (true) // si la connexion est établie
 {
 
 
@@ -25,15 +28,22 @@ while (true)
 
 
     var buffer = new byte[1024];
+    // Initialisation d'une memoire tampon de 1024 bytes afin de stocker l'accusé de reception.
     var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    // Encodage du message en UTF8 (Binaire).
     var reponse = Encoding.UTF8.GetString(buffer, 0, received);
+    // Décodage du buffer en string
 
         Console.WriteLine($"Client : \"{ipAddress.MapToIPv4()}\" sur le port : \"{iPEndPoint.Port}\" connecté au serveur.");
         Console.WriteLine($"Le serveur a reçu le message : \"{reponse}\"");
 
         var adrMessage = "Hello Client !";
+        // Definition de l'accusé de reception
         var echoBytes = Encoding.UTF8.GetBytes(adrMessage);
+        // Encodage du message en UTF8 (Binaire).
         await handler.SendAsync(echoBytes, 0);
+        // Envoi de l'accusé de reception au client
+
         Console.WriteLine($"Le serveur a envoyer l'accusé de reception au client : \"{adrMessage}\"");
         Console.ReadLine();
         break;
